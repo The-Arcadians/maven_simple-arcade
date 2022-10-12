9e823c5e00589da01ec6ba9c940756c71359a19c
Leon Hunter <xleonhunter@gmail.com>
2022-10-11 20:57:18 -0400

    reverted implementation

--- src/main/java/com/github/curriculeon/arcade/numberguess/NumberGuessGame.java
+++ src/main/java/com/github/curriculeon/arcade/numberguess/NumberGuessGame.java
@@ -4,8 +4,4 @@ package com.github.curriculeon.arcade.numberguess;
  * Created by leon on 7/21/2020.
  */
 public class NumberGuessGame {
-    public void run() {
-        System.out.println("Hello world");
-        System.out.println("Running the number guess game");
-    }
 }
