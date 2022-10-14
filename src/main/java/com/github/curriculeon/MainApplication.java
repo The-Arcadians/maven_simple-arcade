61976e4d08efe749f31f1f412143163ade224c86
Brent <BrentC0328@gmail.com>
2022-10-14 18:38:20 -0400

    NumberGuessGame base complete

--- src/main/java/com/github/curriculeon/MainApplication.java
+++ src/main/java/com/github/curriculeon/MainApplication.java
@@ -1,7 +1,13 @@
 package com.github.curriculeon;
 
+import com.github.curriculeon.arcade.numberguess.NumberGuessGame;
+
 public class MainApplication {
-    public static void main(String[] args) {
-        new Arcade().run();
+//    public static void main(String[] args) {
+//        new Arcade().run();
+//    }
+
+    public static void main(String[] args){
+        new NumberGuessGame().run();
     }
 }
