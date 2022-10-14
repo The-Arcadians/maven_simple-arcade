5c6c9bc252c37e57f4d67e44acce0a25791b816e
Eblackk <aps14blacerig@gmail.com>
2022-10-13 21:03:51 -0400

    Updated arcadeAccount

--- src/main/java/com/github/curriculeon/arcade/ArcadeAccount.java
+++ src/main/java/com/github/curriculeon/arcade/ArcadeAccount.java
@@ -6,4 +6,33 @@ package com.github.curriculeon.arcade;
  * The `ArcadeAccount` is used to log into the system to select a `Game` to play.
  */
 public class ArcadeAccount {
+    public String accountName;
+    public String accountPassword;
+    public ArcadeAccountManager arcadeAccountManager;
+
+    public String getAccountName() {
+        String userAccountName = new String("");
+        return accountName;
+    }
+
+    public String getAccountPassword() {
+        String userAccountPassword = new String("");
+        return accountPassword;
+    }
+
+    public ArcadeAccountManager getArcadeAccountManager() {
+        return arcadeAccountManager;
+    }
+
+    public void setAccountName(String accountName) {
+        this.accountName = accountName;
+    }
+
+    public void setAccountPassword(String accountPassword) {
+        this.accountPassword = accountPassword;
+    }
+
+    public void setArcadeAccountManager(ArcadeAccountManager arcadeAccountManager) {
+        this.arcadeAccountManager = arcadeAccountManager;
+    }
 }
