50a1757a12b01305fca726863a059e39b6fbc364
Eblackk <aps14blacerig@gmail.com>
2022-10-18 19:53:59 -0400

    updated slotsGame

--- src/main/java/com/github/curriculeon/MainApplication.java
+++ src/main/java/com/github/curriculeon/MainApplication.java
@@ -1,13 +1,15 @@
 package com.github.curriculeon;
 
 import com.github.curriculeon.arcade.numberguess.NumberGuessGame;
+import com.github.curriculeon.arcade.slots.SlotsGame;
 
 public class MainApplication {
-//    public static void main(String[] args) {
-//        new Arcade().run();
-//    }
-
-    public static void main(String[] args){
-        new NumberGuessGame().run();
+    public static void main(String[] args) {
+        new SlotsGame().run();
+        //new Arcade().run();
     }
-}
+
+   // public static void main(String[] args) {
+        //new NumberGuessGame().run();
+    }
+
--- src/main/java/com/github/curriculeon/arcade/slots/SlotsGame.java
+++ src/main/java/com/github/curriculeon/arcade/slots/SlotsGame.java
@@ -6,11 +6,32 @@ import com.github.curriculeon.arcade.PlayerInterface;
 import com.github.curriculeon.utils.AnsiColor;
 import com.github.curriculeon.utils.IOConsole;
 
+import java.util.Random;
+import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
+
 /**
  * Created by leon on 7/21/2020.
  */
 public class SlotsGame implements GameInterface {
     private final IOConsole logger = new IOConsole(AnsiColor.YELLOW);
+
+
+    public final Character[] matrix;
+
+    public SlotsGame() {
+        this.matrix = new Character[3];
+
+    }
+
+
+    public Boolean slotMachine(Character slotMachine) {
+        Character slot1 = this.matrix[0];
+        Character slot2 = this.matrix[1];
+        Character slot3 = this.matrix[2];
+        return null;
+    }
+
 
     @Override
     public void add(PlayerInterface player) {
@@ -22,22 +43,54 @@ public class SlotsGame implements GameInterface {
 
     }
 
+
     @Override
     public void run() {
+
         System.out.println("Welcome to the Slot Machine!");
         //want to input the players username in the message after "Good Luck"
-        System.out.println("\nGame Start! You will begin with $50.00" + "Good Luck!");
-        // spin
-        //Generate a random sequence using the following "Apple","Apple","Apple", "Orange","Orange","Orange"
-        // "Grapes","Grapes","Grapes", "Cherry","Cherry","Cherry", "banana","banana","banana",
-        // "Kiwi","Kiwi","Kiwi"
-        // Created multiples to up the odds
+        System.out.println("\nGame Start! You will begin with $50.00 " + "Good Luck!");
+        int Balance = 50;
+        int bet = 0, remainingBalance = 0, winnings1, winnings2;
+        //int slot1, slot2, slot3;
+        //ThreadLocalRandom randomNumberGenerator = ThreadLocalRandom.current();
 
-        // Winning Solutions = 3 consecutive fruit objects
+        // Random number generator
+        Random generator = new Random();
 
+        // need to be able to place a bet
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Place your bet $");
+        bet = scanner.nextInt();
+
+
+// Winning Solutions = 3 of the same numbers || 2 of the same number win half
+        if (bet > 0 && bet < Balance) {
+            Character slot1 = this.matrix[0];
+            Character slot2 = this.matrix[1];
+            Character slot3 = this.matrix[2];
+
+
+            if (slot1 == slot2 && slot2 == slot3 && slot3 == slot1) {
+                System.out.println("Jackpot!: " + (double) (slot1 + 1) * bet);
+                winnings1 = (slot1 + 1) * bet;
+                remainingBalance = Balance - bet + winnings1;
+            } else if (slot1 == slot2 || slot2 == slot3) {
+                System.out.println("You win!: " + (double) (slot2 * bet) / 2);
+                winnings2 = (slot2 * bet) / 2;
+                remainingBalance = Balance - bet + winnings2;
+
+            } else {
+                System.out.println("Balance " + (double) (Balance - bet));
+            }
+        } else if (bet == 0) {
+            System.out.println("You earned" + remainingBalance);
+
+        }
 
 
     }
+
 
     @Override
     public String printInstructions() {
