3c08c5df335624c2c0dd9a37054fecef5dc367ed
Leon Hunter <xleonhunter@gmail.com>
2022-10-17 20:01:06 -0400

    updated tictactoe

--- src/main/java/com/github/curriculeon/arcade/tictactoe/TicTacToeGame.java
+++ src/main/java/com/github/curriculeon/arcade/tictactoe/TicTacToeGame.java
@@ -1,4 +1,21 @@
 package com.github.curriculeon.arcade.tictactoe;
 
-public class TicTacToeGame {
+import com.github.curriculeon.arcade.GameInterface;
+import com.github.curriculeon.arcade.PlayerInterface;
+
+public class TicTacToeGame implements GameInterface {
+    @Override
+    public void add(PlayerInterface player) {
+
+    }
+
+    @Override
+    public void remove(PlayerInterface player) {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
 }
