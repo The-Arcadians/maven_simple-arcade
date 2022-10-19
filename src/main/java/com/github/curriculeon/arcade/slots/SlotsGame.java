653a1a2d05033ac4ecb3d9213ee0463ffbd6bb8e
Eblackk <aps14blacerig@gmail.com>
2022-10-18 22:01:21 -0400

    updated slotGame

--- src/main/java/com/github/curriculeon/arcade/slots/SlotsGame.java
+++ src/main/java/com/github/curriculeon/arcade/slots/SlotsGame.java
@@ -1,6 +1,5 @@
 package com.github.curriculeon.arcade.slots;
 
-import com.github.curriculeon.arcade.ArcadeAccount;
 import com.github.curriculeon.arcade.GameInterface;
 import com.github.curriculeon.arcade.PlayerInterface;
 import com.github.curriculeon.utils.AnsiColor;
@@ -8,7 +7,6 @@ import com.github.curriculeon.utils.IOConsole;
 
 import java.util.Random;
 import java.util.Scanner;
-import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * Created by leon on 7/21/2020.
@@ -26,9 +24,9 @@ public class SlotsGame implements GameInterface {
 
 
     public Boolean slotMachine(Character slotMachine) {
-        Character slot1 = this.matrix[0];
-        Character slot2 = this.matrix[1];
-        Character slot3 = this.matrix[2];
+        Character slot1Position = this.matrix[0];
+        Character slot2Position = this.matrix[1];
+        Character slot3Position = this.matrix[2];
         return null;
     }
 
@@ -44,6 +42,7 @@ public class SlotsGame implements GameInterface {
     }
 
 
+
     @Override
     public void run() {
 
@@ -52,43 +51,59 @@ public class SlotsGame implements GameInterface {
         System.out.println("\nGame Start! You will begin with $50.00 " + "Good Luck!");
         int Balance = 50;
         int bet = 0, remainingBalance = 0, winnings1, winnings2;
-        //int slot1, slot2, slot3;
+        int sp0, sp1, sp2;
         //ThreadLocalRandom randomNumberGenerator = ThreadLocalRandom.current();
-
-        // Random number generator
-        Random generator = new Random();
 
         // need to be able to place a bet
         Scanner scanner = new Scanner(System.in);
         System.out.println("Place your bet $");
         bet = scanner.nextInt();
 
+         sp0 = spin(3);
+         sp1 = spin(3);
+         sp2 = spin(3);
+
+        // Random number generator
+        Random generator = new Random();
+
+
+
+
 
 // Winning Solutions = 3 of the same numbers || 2 of the same number win half
-        if (bet > 0 && bet < Balance) {
-            Character slot1 = this.matrix[0];
-            Character slot2 = this.matrix[1];
-            Character slot3 = this.matrix[2];
+        //if (bet > 0 && bet < Balance) {
+            //Character slot1Position = this.matrix[0];
+            //Character slot2Position = this.matrix[1];
+            //Character slot3Position = this.matrix[2];
 
 
-            if (slot1 == slot2 && slot2 == slot3 && slot3 == slot1) {
-                System.out.println("Jackpot!: " + (double) (slot1 + 1) * bet);
-                winnings1 = (slot1 + 1) * bet;
-                remainingBalance = Balance - bet + winnings1;
-            } else if (slot1 == slot2 || slot2 == slot3) {
-                System.out.println("You win!: " + (double) (slot2 * bet) / 2);
-                winnings2 = (slot2 * bet) / 2;
-                remainingBalance = Balance - bet + winnings2;
 
-            } else {
-                System.out.println("Balance " + (double) (Balance - bet));
-            }
-        } else if (bet == 0) {
-            System.out.println("You earned" + remainingBalance);
+
+            //if (slot1Position == slot2Position && slot2Position == slot3Position && slot3Position == slot1Position) {
+               // System.out.println("Jackpot!: " + (double) (slot1Position) * bet);
+               // winnings1 = (slot1Position + 1) * bet;
+                //remainingBalance = Balance - bet + winnings1;
+            //} else if (slot1Position == slot2Position || slot2Position == slot3Position) {
+               // System.out.println("You win!: " + (double) (slot2Position * bet) / 2);
+              //  winnings2 = (slot2Position * bet) / 2;
+               // remainingBalance = Balance - bet + winnings2;
+
+            //} else {
+                //System.out.println("Balance " + (double) (Balance - bet));
+            //}
+        //} else if (bet == 0) {
+            //System.out.println("You earned" + remainingBalance);
 
         }
 
 
+    //}
+
+    private int spin(int i) {
+        int num;
+        Random random = new Random();
+        num = random.nextInt(20);
+        return num;
     }
 
 
