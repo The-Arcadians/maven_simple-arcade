78a1959b4f5114d8b401b2a8cc47d1db0ddebcd3
Eblackk <aps14blacerig@gmail.com>
2022-10-18 22:43:46 -0400

    updated conflicting error to MainApplication

--- src/main/java/com/github/curriculeon/MainApplication.java
+++ src/main/java/com/github/curriculeon/MainApplication.java
@@ -1,15 +1,15 @@
 package com.github.curriculeon;
 
 import com.github.curriculeon.arcade.numberguess.NumberGuessGame;
-import com.github.curriculeon.arcade.slots.SlotsGame;
+import com.github.curriculeon.arcade.tictactoe.TicTacToe;
 
 public class MainApplication {
-    public static void main(String[] args) {
-        new SlotsGame().run();
-        //new Arcade().run();
-    }
+//    public static void main(String[] args) {
+//        new Arcade().run();
+//    }
 
-   // public static void main(String[] args) {
-        //new NumberGuessGame().run();
+    public static void main(String[] args){
+        new TicTacToe().run();
     }
+}
 
