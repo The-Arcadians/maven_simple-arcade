c698e05110878cd794a5d87a37f81024d9ee3709
Brent <BrentC0328@gmail.com>
2022-10-22 15:59:14 -0400

    reset main application

--- src/main/java/com/github/curriculeon/MainApplication.java
+++ src/main/java/com/github/curriculeon/MainApplication.java
@@ -3,11 +3,8 @@ package com.github.curriculeon;
 import com.github.curriculeon.arcade.numberguess.NumberGuessGame;
 
 public class MainApplication {
-//    public static void main(String[] args) {
-//        new Arcade().run();
-//    }
-
-    public static void main(String[] args){
-        new NumberGuessGame().run();
+    public static void main(String[] args) {
+        new Arcade().run();
     }
+
 }
