7fb9ae4b65f20bf6a3534fe9e302cf43974a0b4b
randy <randyr_1993@yahoo.com>
2022-10-24 04:41:17 -0400

    Created classes to run & test games individually. No more merge conflicts :)

---
+++ src/main/java/com/github/curriculeon/arcade/numberguess/NumberGuessGameTest.java
@@ -0,0 +1,10 @@
+package com.github.curriculeon.arcade.numberguess;
+
+/**
+ * Use this class to run and test NumberGuessGame individually
+ */
+public class NumberGuessGameTest {
+    public static void main(String[] args) {
+        new NumberGuessGame().run();
+    }
+}
---
+++ src/main/java/com/github/curriculeon/arcade/tictactoe/TicTacToeTest.java
@@ -0,0 +1,10 @@
+package com.github.curriculeon.arcade.tictactoe;
+
+/**
+ * Use this class to run and test TicTacToe game individually
+ */
+public class TicTacToeTest {
+    public static void main(String[] args) {
+        new TicTacToe().run();
+    }
+}
